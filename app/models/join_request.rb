4312a329e4eb1fcdb3c073afe54f071f0594b9ee
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-02-26 23:44:20 -0300

    Fixing errors/warnings after migrating to Rails 3.2.

--- app/models/join_request.rb
+++ app/models/join_request.rb
@@ -1,4 +1,4 @@
 class JoinRequest < Admission
   validates_presence_of :candidate_id, :candidate_type
-
+  attr_accessible :candidate, :group, :email, :comment, :processed, :accepted, :role_id, :introducer
 end
